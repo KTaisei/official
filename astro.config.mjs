2103bcc2b0d8c3239a0e01ef306b60c475502bed
KTaisei <mathlike.science@gmail.com>
2024-12-15 13:13:50 +0900

    baseの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://KTaisei.com/official',
+	site: 'https://KTaisei.com',
 	integrations: [mdx(), sitemap()],
+	base: "/official",
 });
