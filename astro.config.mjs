7be5b87f457af7eb3f4c5ec35621531b0f3078ff
KTaisei <mathlike.science@gmail.com>
2024-12-15 13:22:33 +0900

    siteのURLさっきは間違えていたから無理やったけど、今回はいけるはず

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://KTaisei.github.io',
+	site: 'https://KTaisei.github.io/official',
 	integrations: [mdx(), sitemap()],
-	base: "/official",
 });
