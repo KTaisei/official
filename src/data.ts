802dfa2f3487ebd1bea3191eeccddc338303d75e
KTaisei <ktaisei@users.noreply.github.com>
2026-06-01 17:26:18 +0900

    画像の適用化

--- src/data.ts
+++ src/data.ts
@@ -6,7 +6,7 @@ export const projects = [
     title: 'Stophone',
     description: 'This application was created to prevent people from using their smartphones while walking.',
     technologies: ['Flutter', 'Dart'],
-    image: 'https://ktaisei.xsrv.jp/blog/walking_phone.jpg',
+    image: '../public/stophone.jpeg',
     github: 'https://github.com/KTaisei/stophone',
     demo: 'https://ktaisei.github.io/stophone_tsx/'
   },
@@ -15,7 +15,7 @@ export const projects = [
     title: 'Math_Battle',
     description: "This program allows you to have a calculation battle with your computer's CPU.",
     technologies: ["Python", "HTML"],
-    image: 'http://ktaisei.xsrv.jp/blog/Designer.jpg',
+    image: '../mathbattle.jpg',
     github: 'https://github.com/KTaisei/math_Battle',
     demo: 'https://github.com/KTaisei/math_Battle'
   }
